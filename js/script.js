d63008287eaf5bd50ea45edc2c7bffda7cf14e67
renata.igarashi <rsigarashi@gmail.com>
2024-01-04 12:20:12 -0300

    chore: format js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,14 @@
 let getAdvice = async () => {
     let response = await fetch('https://api.adviceslip.com/advice')
-    
     const data = await response.json()
-    
-    return document.querySelector('#advice').textContent = `"${data.slip.advice}"`
- }
 
-let advice =  document.querySelector('#advice').textContent 
+    return document.querySelector('#advice').textContent = `"${data.slip.advice}"`
+}
+
+let advice = document.querySelector('#advice').textContent
 advice = getAdvice()
 
-
 let getAdviceButton = document.querySelector('#get-advice')
-
 
 getAdviceButton.addEventListener('click', getAdvice)
 
