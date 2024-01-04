4e0311f058e429fda38e0a3e0c2cc6751707caf9
renata.igarashi <rsigarashi@gmail.com>
2024-01-04 12:18:47 -0300

    feat: create api advice page

---
+++ js/script.js
@@ -0,0 +1,18 @@
+let getAdvice = async () => {
+    let response = await fetch('https://api.adviceslip.com/advice')
+    
+    const data = await response.json()
+    
+    return document.querySelector('#advice').textContent = `"${data.slip.advice}"`
+ }
+
+let advice =  document.querySelector('#advice').textContent 
+advice = getAdvice()
+
+
+let getAdviceButton = document.querySelector('#get-advice')
+
+
+getAdviceButton.addEventListener('click', getAdvice)
+
+
